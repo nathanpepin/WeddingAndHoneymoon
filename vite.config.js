263604b05c910bb09cd7fea94412ebecc50451e1
nathanpepin <46303125+nathanpepin@users.noreply.github.com>
2025-08-03 19:04:08 -0400

    Setup GitHub Pages deployment with gh-pages package and correct base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WeddingAndHoneymoon/',
+  base: '/',
   server: {
     host: true,
     port: 3000
